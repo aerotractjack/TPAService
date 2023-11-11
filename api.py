6784e2c358d638d12d57597b673affe737f6f47d
jack <jackw@aerotractone.com>
2023-11-11 15:07:03 -0800

    work

--- api.py
+++ api.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_executor import Executor
 from flasgger import Swagger
+import sys
 from docs import template, swag_submit_tpa
 from TPACalc import TPAReport, integration
 
 app = Flask(__name__)
 executor = Executor(app)
 swagger = Swagger(app, template=template)
+
+def log(msg):
+    print(msg)
+    sys.stdout.flush()
 
 @app.route("/tpa", methods=['POST'])
 @swag_submit_tpa
@@ -15,12 +20,15 @@ def tpa():
     API endpoint to submit TPA calculation requests for validated data
     '''
     contents = request.get_json()
-    contents = {k.lower(): v for k,v in data.items()}
+    contents = {k.lower(): v for k,v in contents.items()}
     try:
+        # TPAReport(**contents)
         executor.submit(TPAReport, **contents)
         paths = integration.get_tpa_paths(
             contents['client_id'], contents['project_id'], contents['stand_id']
         )
+        log(contents)
+        log("==============")
         return jsonify(paths)
     except Exception as e:
         print(e)
