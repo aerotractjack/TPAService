556bce08de2e3e5262d2007e87c827f56d274385
jack <jackw@aerotractone.com>
2023-11-11 14:41:10 -0800

    init

---
+++ api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from flask_executor import Executor
+from flasgger import Swagger
+from docs import template, swag_submit_tpa
+from TPACalc import TPAReport, integration
+
+app = Flask(__name__)
+executor = Executor(app)
+swagger = Swagger(app, template=template)
+
+@app.route("/tpa", methods=['POST'])
+@swag_submit_tpa
+def tpa():
+    contents = request.get_json()
+    contents = **{k.lower(): v for k,v in data.items()}
+    try:
+        executor.submit(TPAReport, contents)
+        paths = integration.get_tpa_paths(
+            contents['client_id'], contents['project_id'], contents['stand_id']
+        )
+        return jsonify(paths)
+    except Exception as e:
+        print(e)
+        sys.stdout.flush()
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(port=7113, debug=True, host="0.0.0.0")
---
+++ docs.py
@@ -0,0 +1,60 @@
+from flasgger import swag_from
+
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "TPA Estimate Service API Documentation",
+        "description": "API documentation for the Aerotract LabelService tool",
+        "version": "1.0.0"
+    }
+}
+
+swag_submit_tpa = swag_from({
+    'responses': {
+        200: {
+            'description': 'Succesfully submitted operation',
+            'schema': {
+                'type': 'string',
+            }
+        },
+        500: {
+            'description': 'Failed to submit operation',
+            'schema': {
+                'type': 'string',
+            }
+        }
+    },
+    'parameters': [
+        {
+            'name': 'client_id',
+            'description': 'Client ID of data to process',
+            'example': '10007',
+            'in': 'body',
+            'required': True,
+            'type': 'string'
+        },{
+            'name': 'project_id',
+            'description': 'Project ID of data to process',
+            'example': '101036',
+            'in': 'body',
+            'required': True,
+            'type': 'string'
+        },{
+            'name': 'stand_id',
+            'description': 'Stand ID of data to process',
+            'example': '103',
+            'in': 'body',
+            'required': True,
+            'type': 'string'
+        },
+        {
+            'name': 'desired_confidence',
+            'description': 'Desired confidence level of TPA estimate',
+            'example': 0.95,
+            'in': 'body',
+            'required': False,
+            'type': 'number',
+            'format': 'float'
+        },
+    ]
+})
